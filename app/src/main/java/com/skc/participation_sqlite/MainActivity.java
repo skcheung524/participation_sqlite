7e6b8084139a3438674b8a2a92c696e3b17f3dcc
Kei Cheung <scheung@css.edu>
2018-04-03 05:36:03 -0500

    Initial commit

---
+++ app/src/main/java/com/skc/participation_sqlite/MainActivity.java
@@ -0,0 +1,68 @@
+package com.skc.participation_sqlite;
+
+import java.util.List;
+import java.util.Random;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+    private CommentsDataSource datasource;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datasource = new CommentsDataSource(this);
+        datasource.open();
+
+        List<Comment> values = datasource.getAllComments();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
+        Comment comment = null;
+        switch (view.getId()) {
+            case R.id.add: //press add button
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = datasource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete: //press delete button
+                if (getListAdapter().getCount() > 0) {
+                    comment = (Comment) getListAdapter().getItem(0);
+                    datasource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() { //on going back to app re-open the closed connection
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() { // when getting a call activity is paused, closed db connection
+        datasource.close();
+        super.onPause();
+    }
+
+}
---
+++ app/src/main/java/com/skc/participation_sqlite/MySQLiteHelper.java
@@ -0,0 +1,42 @@
+package com.skc.participation_sqlite;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_COMMENTS = "comments"; //table name
+    public static final String COLUMN_ID = "_id"; //pk
+    public static final String COLUMN_COMMENT = "comment"; //column name
+
+    private static final String DATABASE_NAME = "commments.db"; //the file db is stored as
+    private static final int DATABASE_VERSION = 1; //this is used to connect to the specific version
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COMMENTS + "( " + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_COMMENT
+            + " text not null);";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE); //used to execute the statement
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS); //drop table is dangerous and will delete everything
+        onCreate(db);
+    }
+
+}
